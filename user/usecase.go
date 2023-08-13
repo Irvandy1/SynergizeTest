98782db0d594f0dad12e1985a42a14a013984dfe
Irvandy Hartono <irvandy.hartono@Koinworks.com>
2023-08-13 18:27:13 +0700

    tidy up

--- user/usecase.go
+++ user/usecase.go
@@ -3,7 +3,6 @@ package user
 import (
 	"SynergizeTest/config"
 	"SynergizeTest/models"
-	"context"
 	"fmt"
 	"strconv"
 	"time"
@@ -13,17 +12,14 @@ import (
 	"gorm.io/gorm"
 )
 
-func CreateUserSession(ctx context.Context, redisClient redis.Client, user models.User) {
-	keys := fmt.Sprintf("user%v", user.UserID)
-	redisClient.Set(keys, "true", 3600)
-}
-
 func Register(ctx *gin.Context, form models.User) (err error) {
 	form.RegisterAt = time.Now()
+	//add user
 	if errx := config.DB.Create(&form); errx.Error != nil {
 		err = errx.Error
 		return
 	}
+	// automatically create wallet when user register
 	err = CreateWallet(ctx, models.Wallet{
 		Saldo:     0,
 		UserID:    form.UserID,
@@ -36,6 +32,7 @@ func Register(ctx *gin.Context, form models.User) (err error) {
 }
 
 func CreateWallet(ctx *gin.Context, Wallet models.Wallet) (err error) {
+	//add wallet
 	errx := config.DB.Create(&Wallet)
 	if errx.Error != nil {
 		err = errx.Error
@@ -46,6 +43,7 @@ func CreateWallet(ctx *gin.Context, Wallet models.Wallet) (err error) {
 
 func Login(ctx *gin.Context, form models.LoginForm) (err error) {
 	user := models.User{}
+	//check if username and password correct
 	if errx := config.DB.Where("user_name = ? AND password = ?", form.Username, form.Password).Find(&user); errx.Error != nil {
 		err = errx.Error
 		return
@@ -56,7 +54,7 @@ func Login(ctx *gin.Context, form models.LoginForm) (err error) {
 	}
 
 	userID := strconv.Itoa(user.UserID)
-
+	//set user login session in redis
 	config.Redis.Set(userID, "true", 86400*time.Second)
 
 	return
@@ -64,20 +62,12 @@ func Login(ctx *gin.Context, form models.LoginForm) (err error) {
 
 func Logout(ctx *gin.Context, form models.LogoutForm) {
 	userID := strconv.Itoa(form.UserID)
+	//remove user login
 	config.Redis.Set(userID, "false", 10*time.Second)
 }
 
 func RegisterBank(ctx *gin.Context, form models.Bank) (err error) {
-	form.UpdatedAt = time.Now()
-	rows := config.DB.Create(&form)
-	if rows.Error != nil {
-		err = rows.Error
-		return
-	}
-	return
-}
-
-func TopUp(ctx *gin.Context, form models.TopUpForm) (err error) {
+	//check if user login
 	userID := strconv.Itoa(form.UserID)
 	res, err := config.Redis.Get(userID).Result()
 	if err != nil && err != redis.Nil {
@@ -88,19 +78,37 @@ func TopUp(ctx *gin.Context, form models.TopUpForm) (err error) {
 		return
 	}
 
-	var user models.User
-	var wallet models.Wallet
-	if errx := config.DB.Where("user_id = ?", form.UserID).Find(&user); errx.Error != nil {
-		err = errx.Error
-		fmt.Println(errx)
+	form.UpdatedAt = time.Now()
+	//add bank
+	rows := config.DB.Create(&form)
+	if rows.Error != nil {
+		err = rows.Error
 		return
 	}
+	return
+}
+
+func TopUp(ctx *gin.Context, form models.TopUpForm) (err error) {
+	//check if user login
+	userID := strconv.Itoa(form.UserID)
+	res, err := config.Redis.Get(userID).Result()
+	if err != nil && err != redis.Nil {
+		return
+	}
+	if res != "true" || err == redis.Nil {
+		err = fmt.Errorf("user not login")
+		return
+	}
+
+	var wallet models.Wallet
+	//get user wallet
 	if errx := config.DB.Where("user_id=?", form.UserID).Find(&wallet); errx.Error != nil {
 		err = errx.Error
 		fmt.Println(errx)
 		return
 	}
 
+	//add amount to current saldo in db and save
 	wallet.Saldo = wallet.Saldo + form.Amount
 	if errx := config.DB.Save(&wallet); errx.Error != nil {
 		err = errx.Error
@@ -111,6 +119,7 @@ func TopUp(ctx *gin.Context, form models.TopUpForm) (err error) {
 }
 
 func GetUserDetail(ctx *gin.Context, form models.LogoutForm) (res models.UserDetail, err error) {
+	//check user login
 	userID := strconv.Itoa(form.UserID)
 	redisResult, err := config.Redis.Get(userID).Result()
 	if err != nil && err != redis.Nil {
@@ -120,6 +129,7 @@ func GetUserDetail(ctx *gin.Context, form models.LogoutForm) (res models.UserDet
 		err = fmt.Errorf("user not login")
 		return
 	}
+	//get all user data
 	if errx := config.DB.Where("user_id = ?", form.UserID).Find(&res.User); errx.Error != nil {
 		err = errx.Error
 		fmt.Println(errx)
@@ -142,6 +152,7 @@ func GetUserList(ctx *gin.Context, form models.Pagination) (res []models.UserDet
 	var user []models.User
 	var rows *gorm.DB
 	filter := ""
+	//check filter for user table
 	IsExist := IsExists(form.Keys, []string{"user_name", "email", "phone_number"})
 	if IsExist {
 		filter = fmt.Sprintf("%v = ?", form.Keys)
@@ -162,6 +173,7 @@ func GetUserList(ctx *gin.Context, form models.Pagination) (res []models.UserDet
 		res = append(res, models.UserDetail{
 			User: user[i],
 		})
+		//check filter for wallet column
 		if form.Keys == "saldo" {
 			filter := "user_id = ? and saldo = ?"
 			rows = config.DB.Debug().Where(filter, user[i].UserID, form.Value).Find(&res[i].Wallet)
@@ -176,6 +188,7 @@ func GetUserList(ctx *gin.Context, form models.Pagination) (res []models.UserDet
 				return
 			}
 		}
+		//check filter for bank column
 		IsExist2 := IsExists(form.Keys, []string{"bank_code", "account_number", "account_name"})
 		if IsExist2 {
 			filter = fmt.Sprintf("user_id = ? and %v = ?", form.Keys)
@@ -192,6 +205,7 @@ func GetUserList(ctx *gin.Context, form models.Pagination) (res []models.UserDet
 			}
 		}
 	}
+	// add all valid data
 	var temp []models.UserDetail
 	for i := 0; i < len(res); i++ {
 		IsExist = IsExists(form.Keys, []string{"bank_code", "account_number", "account_name"})
@@ -205,13 +219,6 @@ func GetUserList(ctx *gin.Context, form models.Pagination) (res []models.UserDet
 
 	}
 	return temp, nil
-}
-
-func RemoveIndex(s []models.UserDetail, index int) []models.UserDetail {
-	if len(s) > 1 {
-		return append(s[:index], s[index+1:]...)
-	}
-	return nil
 }
 
 func IsExists(value string, array []string) (exists bool) {
